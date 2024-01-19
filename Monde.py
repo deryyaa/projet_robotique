e3825b393616f4ee88b35bac214eb03e36482d7d
Rasheequa BAGADAD SAIB <rasheequa.m@gmail.com>
2024-01-19 17:04:43 +0100

    ajout de commentaire et ajout d'autre test pour la classe Monde

--- Monde.py
+++ Monde.py
@@ -2,7 +2,7 @@ from Robot import Robot
 class Monde:
     def __init__(self,ligne,colonne):
         """ constructeur """
-        self.ligne=ligne
+        self.ligne=ligne # initialisation des coordonnes
         self.colonne=colonne
         self.robot=None
 
@@ -19,15 +19,27 @@ class Monde:
             a+="|\n"
 
         a+="+"+"-"*self.colonne+"+"+"\n"
-        print(a)
+        print(a) 
+
     def setRobot(self, robot):
-        self.robot=robot
+        self.robot=robot # initialisation du robot
 
+#TEST
 
-robot= Robot(1,20)
+robot= Robot(1,20) # creation d'un robot en point x, y
+m1 = Monde(9,45) # creation d'un monde x*y
+m1.setRobot(robot)
+m1.affiche() # affiche le monde
+robot.avancer(5,5) # fait avancer le robot
+m1.setRobot(robot)
+m1.affiche()
+
+# AUTRE TEST 
+# manque la condition pour ne pas depasser le mur à avancer (rash)
+robot= Robot(0,0)
 m1 = Monde(9,45)
 m1.setRobot(robot)
 m1.affiche()
-robot.avancer(5,5)
-m1.setRobot(robot)
-m1.affiche()
+for i in range (20):
+    robot.avancer(1,1)
+    m1.affiche()
--- Robot.py
+++ Robot.py
@@ -1,5 +1,5 @@
 class Robot:
-    def __init__(self,x,y,vitesse=0): #Constructeur
+    def __init__(self,x,y,vitesse=0): # Constructeur
         self.vitesse=vitesse
         self.x=x
         self.y=y
@@ -7,12 +7,12 @@ class Robot:
     pass
 
     def avancer(self,x1,y1):
-        """fonction qui fais avancer le robot"""
+        """fonction qui fait avancer le robot"""
         self.x+=x1
         self.y+=y1
 
 
-#TEST
+# TEST
 robot1 = Robot(5,5,5)
 robot2 = Robot(5,5)
 print(robot1.x,robot1.y)
