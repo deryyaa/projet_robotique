ac8b18ea9f2844b4362a3a4f0918880afbaae9f2
Rasheequa BAGADAD SAIB <rasheequa.m@gmail.com>
2024-01-19 19:21:25 +0100

    supression des jeux de test et modification de la fonction avancer

--- Robot.py
+++ Robot.py
@@ -6,27 +6,10 @@ class Robot:
         pass
     pass
 
-    def avancer(self,x1,y1):
+    def avancer(self,m, x1,y1):
         """fonction qui fait avancer le robot"""
-        self.x+=x1
-        self.y+=y1
-
-
-# TEST
-# creation des robots
-robot1 = Robot(5, 5)
-robot2 = Robot(5, 5)
-
-# affichage des positions initiales
-print("robot 1 :", robot1.x, robot1.y)
-print("robot 2 :", robot2.x, robot2.y)
-
-# on fait avancer le premier robot
-robot1.avancer(1, 1)
-
-print("apres deplacement")
-print("robot 1 :", robot1.x, robot1.y)
-print("robot 2 :", robot2.x, robot2.y)
-
-# affichage de la vitesse du deuxieme robot
-print("vitesse robot 2 :", robot2.vitesse)
+        if (self.x + x1 >= m.ligne) or (self.y + y1 >= m.colonne): # test si le deplacement est possible 
+            return "mur"
+        else:
+            self.x += x1
+            self.y += y1
