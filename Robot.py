93a2359e2029c37abc657844f80b779518439be9
deryyaa <derya-yaya@outlook.fr>
2024-01-19 16:00:55 +0100

    ajout de la fonction avancer

--- Robot.py
+++ Robot.py
@@ -5,12 +5,17 @@ class Robot:
         self.y=y
         pass
     pass
-    
+
+    def avancer(self,x1,y1):
+        """fonction qui fais avancer le robot"""
+        self.x+=x1
+        self.y+=y1
 
 
 #TEST
 robot1 = Robot(5,5,5)
 robot2 = Robot(5,5)
-
-print(robot1.vitesse)
+print(robot1.x,robot1.y)
+robot1.avancer(1,1)
+print(robot1.x,robot1.y)
 print(robot2.vitesse)
