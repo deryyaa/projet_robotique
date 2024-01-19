94fcd3648974c1b769dcf08e9397ac8cc05ac941
deryyaa <derya-yaya@outlook.fr>
2024-01-19 15:28:48 +0100

    rajout des coordonéee

--- Robot.py
+++ Robot.py
@@ -1,12 +1,15 @@
 class Robot:
-    def __init__(self, vitesse=0): #Constructeur
+    def __init__(self,x,y,vitesse=0): #Constructeur
         self.vitesse=vitesse
+        self.x=x
+        self.y=y
         pass
     pass
+    
 
 #TEST
-robot1 = Robot(15)
-robot2 = Robot()
+robot1 = Robot(5,5,5)
+robot2 = Robot(5,5)
 
 print(robot1.vitesse)
 print(robot2.vitesse)
