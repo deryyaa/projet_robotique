c20408c3779656cd2030246867b415639cc8e79f
deryyaa <derya-yaya@outlook.fr>
2024-01-19 14:59:39 +0100

    el mundo del Derya

---
+++ Monde.py
@@ -0,0 +1,20 @@
+class Monde:
+    def __init__(self,ligne,colonne):
+        self.ligne=ligne
+        self.colonne=colonne
+
+    def affiche(self):
+        a="+"+"-"*self.colonne+"+"+"\n"
+        for i in range(self.ligne):
+            a+="|"
+            for j in range(self.colonne):
+                a+=" "
+            a+="|\n"
+
+        a+="+"+"-"*self.colonne+"+"+"\n"
+        print(a)
+
+
+
+m1 = Monde(9,45)
+m1.affiche()
