f3d9d4e01c955b0e7c722b98ecccb0807fe29dae
Rasheequa BAGADAD SAIB <rasheequa.m@gmail.com>
2024-01-19 19:20:58 +0100

    supression des jeux de test

--- Monde.py
+++ Monde.py
@@ -23,23 +23,3 @@ class Monde:
 
     def setRobot(self, robot):
         self.robot=robot # initialisation du robot
-
-#TEST
-
-robot= Robot(1,20) # creation d'un robot en point x, y
-m1 = Monde(9,45) # creation d'un monde x*y
-m1.setRobot(robot)
-m1.affiche() # affiche le monde
-robot.avancer(5,5) # fait avancer le robot
-m1.setRobot(robot)
-m1.affiche()
-
-# AUTRE TEST 
-# manque la condition pour ne pas depasser le mur à avancer (rash)
-robot= Robot(0,0)
-m1 = Monde(9,45)
-m1.setRobot(robot)
-m1.affiche()
-for i in range (20):
-    robot.avancer(1,1)
-    m1.affiche()
