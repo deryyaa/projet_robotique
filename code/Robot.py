57c96a40e66216e38ed49a8480ecf1d05de45394
hugoamadoUPMC <hugoamado1007@gmail.com>
2024-01-25 13:54:54 +0100

    ajout d'une methode avance dans la classe robot ainsi qu'un attribut direction

--- code/Robot.py
+++ code/Robot.py
@@ -1,10 +1,10 @@
+from math import math
 class Robot:
-    def __init__(self,x,y,vitesse=0): # Constructeur
+    def __init__(self,x,y,vitesse=0,dir=0): # Constructeur
         self.vitesse=vitesse
         self.x=x
         self.y=y
-        pass
-    pass
+        self.dir=dir
 
     def avancer(self,m, x1,y1):
         """fonction qui fait avancer le robot"""
@@ -13,3 +13,9 @@ class Robot:
         else:
             self.x += x1
             self.y += y1
+    def avance(self,m):
+        if (self.x+self.vitesse*math.cos(self.dir)>=m.ligne) or (self.y+self.vitesse*math.sin(self.dir)>=m.colonne):
+            return "mur"
+        else:
+            self.x=self.x+self.vitesse*math.cos(self.dir)
+            self.y=self.y+self.vitesse*math.sin(self.dir)
