e706d978420d64b08c381f4c4332056961ea2a94
Somxyx <boudalilsomaya@gmail.com>
2024-01-27 15:24:19 +0100

    creation obsatcle

---
+++ code/Obstacle.py
@@ -0,0 +1,12 @@
+import math
+
+class Obstacle: 
+    obstacle_crees = 0 
+
+    # obstacle immobile
+    def __init__(self,x,y): 
+        self.x=x
+        self.y=y
+        Obstacle.obstacle_crees += 1
+    
+    print("OK")
