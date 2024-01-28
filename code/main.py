3f87ddbc705effab47f46bb08bd1d3228dfef3fb
deryyaa <derya-yaya@outlook.fr>
2024-01-28 14:16:26 +0100

    modification du main, ajout obstacle

--- code/main.py
+++ code/main.py
@@ -11,7 +11,8 @@ obstacle1 = Obstacle(2, 2, 1, 1)
 obstacle2 = Obstacle(8, 18, 1, 1)
 monde.setObstacle(obstacle1)
 monde.setObstacle(obstacle2)
-# Création du robot
+for i in range(5):
+    monde.setObstacle(Obstacle(2+i+1, 2, 1, 1))
 robot1 = Robot(5, 5, 1, 1, 1, 180)
 monde.setRobot(robot1)
 
@@ -19,9 +20,10 @@ for _ in range(100):
     direction = randint(0, 3)  # 0: Avancer, 1: Reculer, 2: Tourner à gauche, 3: Tourner à droite
 
     if direction == 0 and not (robot1.x + 1 == obstacle1.x and robot1.y == obstacle1.y):  # Vérifie si le prochain mouvement ne heurte pas l'obstacle
-        robot1.avancer(1, monde)
+        #robot1.avancer(1, monde)
+        monde.avancer_robot(1,robot1)
     elif direction == 1 and not (robot1.x - 1 == obstacle1.x and robot1.y == obstacle1.y):
-        robot1.reculer(1, monde)
+        monde.avancer_robot(-1,robot1)
     elif direction == 2:
         robot1.tourner_gauche(10)
     elif direction == 3:
