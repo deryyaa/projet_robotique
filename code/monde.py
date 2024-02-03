1468e8081ec2e7e488dc8c0e198488dde061cb44
deryyaa <derya-yaya@outlook.fr>
2024-02-03 16:13:30 +0100

    créeation d'utilitaire pour metre fonction statiqe

--- code/monde.py
+++ code/monde.py
@@ -1,13 +1,13 @@
 from robot import Robot
 from obstacle import Obstacle
-
+from utilitaire import collision_rect
 class Monde:
     def __init__(self, ligne, colonne):
         """ constructeur """
         self.ligne = ligne  # initialisation des coordonnées
         self.colonne = colonne
         self.robot = None
-        self.obstacles = []
+        self.obstacles = [] #création de la liste d'obstacle
     
     def affiche(self):
         """fonction qui permet d'afficher le monde dans le terminal"""
@@ -15,11 +15,11 @@ class Monde:
         verification=False
         for i in range(self.ligne):
             a += "|"
-            for j in range(self.colonne):
-                if self.robot is not None and int(self.robot.x) == i and int(self.robot.y) == j:
+            for j in range(self.colonne): 
+                if self.robot is not None and int(self.robot.x) == i and int(self.robot.y) == j: #vérifie si il y a un robot ou pas 
                     a += "X"
                 else:
-                    for d in self.obstacles:
+                    for d in self.obstacles: #parcours la liste d'obstcle pour tester leur coordonnées et les placées
                         if int(d.x)==i and int(d.y)==j:
                             verification=True
                             a+="0"
@@ -34,7 +34,7 @@ class Monde:
     
     def setRobot(self, robot):
         """Initialise le robot dans le monde"""
-        self.robot = robot
+        self.robot = robot 
 
     def setObstacle(self, obstacle):
         """Initialise un obstacle dans le monde"""
@@ -44,29 +44,23 @@ class Monde:
         """ Vérifie si le robot peut avancer sans dépasser les limites du monde """
         new_x = robot.x + dx
         new_y = robot.y + dy
-        if 0 <= new_x-robot.longueur/2 and new_x-robot.longueur/2 < self.ligne and 0 <= new_y-robot.longueur/2 and new_y+(robot.longueur)/2 < self.colonne:
+        if 0 <= new_x-robot.longueur/2 and new_x-robot.longueur/2 < self.ligne and 0 <= new_y-robot.longueur/2 and new_y+(robot.longueur)/2 < self.colonne:  
             return True
         return False
 
     def avancer_robot(self,distance,robot):
         """ verifie si il n'y a pas d'obstacle sur la position dans lequel le robot va avancer"""
-        tmpx=robot.x
+        tmpx=robot.x #initialise ces positions dans une variable temporaires
         tmpy=robot.y
         robot.avancer(distance,self)
         for i in self.obstacles:
-            if Monde.collision_rect([(i.x-i.longeur/2,i.y-i.largeur/2),(i.x+i.longeur/2,i.y+i.largeur/2)],[(robot.x-robot.longueur/2,robot.y-robot.largeur/2),(robot.x+robot.longueur/2,robot.y+robot.largeur/2)]):
-                #si il y a un obstacle on remet le robot a ces positions temporaire d'avant 
+            if collision_rect([(i.x-i.longeur/2,i.y-i.largeur/2),(i.x+i.longeur/2,i.y+i.largeur/2)],[(robot.x-robot.longueur/2,robot.y-robot.largeur/2),(robot.x+robot.longueur/2,robot.y+robot.largeur/2)]):
+                #si il y a un obstacle on remet le robot a ces positions temporaire d'avant (on le vérifie grâce aux coordonnées)
                 robot.x=tmpx
                 robot.y=tmpy
                 print("le robot vient de percuter le mur")
-                break
+                break 
                 
     
     
-    def collision_rect(r1,r2): #prend en parametre une liste de tuple des 2 coordonnées de mon rectangle (obstacle et robot)
-        """renvoie True quand les 2 rectangle r1,r2 se superpose"""
-        x1,y1,w1,h1 = r1[0][0], r1[0][1], r1[1][0] - r1[0][0], r1[1][1] - r1[0][1]
-        x2,y2,w2,h2= r2[0][0], r2[0][1], r2[1][0] - r2[0][0], r2[1][1] - r2[0][1]
-
-        return [True,False][x1 >= x2 + w2 or x1 + w1 <= x2 or y1 >= y2 + h2 or y1 + h1 <= y2]
-
+  
---
+++ code/utilitaire.py
@@ -0,0 +1,12 @@
+
+
+def collision_rect(r1,r2): #prend en parametre une liste de tuple des 2 coordonnées de mon rectangle (obstacle et robot)
+    """renvoie True quand les 2 rectangle r1,r2 se superpose"""
+    # Déballage des tuples pour obtenir les coordonnées et dimensions des rectangles
+    x1,y1,w1,h1 = r1[0][0], r1[0][1], r1[1][0] - r1[0][0], r1[1][1] - r1[0][1]
+    x2,y2,w2,h2= r2[0][0], r2[0][1], r2[1][0] - r2[0][0], r2[1][1] - r2[0][1]
+    # Vérification de la superposition des rectangles
+    # La superposition est vérifiée en négatif, donc si l'une des conditions est vraie, la superposition n'a pas lieu.
+
+    return [True,False][x1 >= x2 + w2 or x1 + w1 <= x2 or y1 >= y2 + h2 or y1 + h1 <= y2]
+
