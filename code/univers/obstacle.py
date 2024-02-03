aed5c376cffda6cc6a9f0f390aae2c381b1ac9a1
Somxyx <boudalilsomaya@gmail.com>
2024-02-03 19:11:17 +0100

    changement repertoire

---
+++ code/univers/obstacle.py
@@ -0,0 +1,23 @@
+import math
+
+class Obstacle: 
+    """ Classe pour représenter un obstacle dans l'environnement.
+
+        :var classe obstacle_crees: Compteur du nombre total d'obstacles créés
+    """
+    obstacle_crees = 0 
+
+    # obstacle immobile
+    def __init__(self,x,y,largeur,longueur): 
+        """Initialise un nouvel obstacle.
+        :param x: Coordonnée x de l'obstacle 
+        :param y:Coordonnée y de l'obstacle
+        :param largeur: Dimension de l'obsatcle  
+        :param longueur: Dimension de l'obsatcle """
+
+        self.x=x
+        self.y=y
+        self.largeur=largeur
+        self.longeur=longueur
+        Obstacle.obstacle_crees += 1
+
