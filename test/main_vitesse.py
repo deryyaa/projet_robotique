c8d1d4f760c49d0033e332df7afb477748e7c2f3
deryyaa <derya-yaya@outlook.fr>
2024-02-06 15:56:33 +0100

    uniteste de la classe robot

--- test/main_vitesse.py
+++ test/main_vitesse.py
@@ -1,3 +1,4 @@
+import unittest
 from ..robot.robot import Robot
 from ..univers.monde import Monde
 
---
+++ test/test_robot.py
@@ -0,0 +1,41 @@
+import unittest
+import math
+from ..robot.robot import Robot
+from ..univers.monde import Monde
+#python3 -m unittest test/test_robot.py -v
+class Test_Robot(unittest.TestCase):
+    def setUp(self):
+        self.new_robot=Robot(1,2,3,4,5,6)
+        self.new_monde=Monde(20,20)
+
+    def test_init(self):
+        self.assertEqual(self.new_robot.vitesse,1)
+        self.assertEqual(self.new_robot.x,2)
+        self.assertEqual(self.new_robot.y,3)
+        self.assertEqual(self.new_robot.dir,4%360)
+        self.assertEqual(self.new_robot.largeur,5)
+        self.assertEqual(self.new_robot.longeur,6)
+
+    def test_avancer(self):
+        x=self.new_robot.x
+        y=self.new_robot.y
+        self.new_robot.avancer(10,self.new_monde)
+        new_x=self.new_robot.x
+        new_y=self.new_robot.y
+        self.assertEqual(new_x-x,10 * math.cos(math.radians(self.new_robot.dir)))
+        self.assertEqual(new_y-y,10 * math.cos(math.radians(self.new_robot.dir)))
+
+
+    def test_tourner_droite(self):
+        dir=self.new_robot.dir
+        self.new_robot.tourner_droite(13)
+        new_dir=self.new_robot.dir
+        self.assertEqual(new_dir,dir-13%360)
+
+    def test_tourner_gauche(self):
+        dir=self.new_robot.dir
+        self.new_robot.tourner_droite(13)
+        new_dir=self.new_robot.dir
+        self.assertEqual(new_dir,dir+13%360)
+ 
+   
