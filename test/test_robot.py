714ae52beeb1852294b3b92cdf08e95326899ba1
Somxyx <boudalilsomaya@gmail.com>
2024-02-07 10:54:49 +0100

    rajout commande test

--- test/test_robot.py
+++ test/test_robot.py
@@ -5,6 +5,14 @@ from futurama.univers.monde import Monde
 
 #python3 -m unittest test/test_robot.py -v
 
+#EXEMPLE:
+#Pour tout tester :
+#python -m unittest discover test -v
+#Pour tester un fichier :
+#python -m unittest test.test_geo2d -v
+#Pour tester une classe de test :
+#python -m unittest test.test_geo2d.TestVec2D -v
+
 class Test_Robot(unittest.TestCase):
     def setUp(self):
         self.new_robot=Robot(1,2,3,4,5,6)
