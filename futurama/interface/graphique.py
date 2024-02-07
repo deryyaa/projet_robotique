2e20d929f0b0afd2ab40171b41e355699b83e496
hugoamadoUPMC <hugoamado1007@gmail.com>
2024-02-07 10:50:03 +0100

    modification d'interface graphique pour qu'elle fonctionne avec le nouveaux robot

--- futurama/interface/graphique.py
+++ futurama/interface/graphique.py
@@ -4,11 +4,8 @@ from futurama.univers.monde import Monde
 from futurama.univers.obstacle import Obstacle
 import math
 
-# python -m unittest futurama/interface/graphique.py -v
-# executer avec python ou python3
-
 # Création du monde
-monde = Monde(400, 400)
+monde = monde.Monde(400, 400)
 
 # Création de la fenêtre principale
 fenetre = Tk()
@@ -19,11 +16,11 @@ cnv = Canvas(fenetre, width=monde.colonne+20, height=monde.ligne, bg="ivory")
 cnv.pack()
 
 # Création du robot dans le monde
-robot1 = Robot(300, 200, 20, 20)  # Position du robot dans le monde
+robot1 = robot.Robot(300, 200, 20, 20 , 10)  # Position du robot dans le monde
 
 #création de 2 obstacle 
 for i in range(2):
-    monde.setObstacle(Obstacle(2+(i+1)*100, 40, 50, 50)) #creation de plusieurs obstacle pour crée une colision
+    monde.setObstacle(obstacle.Obstacle(2+(i+1)*100, 40, 50, 50)) #creation de plusieurs obstacle pour crée une colision
 for i in monde.obstacles:
     print(i.x,i.y)
     cnv.create_rectangle(i.x-i.longeur/2,i.y-i.largeur/2,i.x+i.longeur/2,i.y+i.largeur/2,fill="grey") #affichage des 2 obstacles
@@ -35,20 +32,15 @@ def dessineRobot(canvas,robot):
     canvas.delete("rectangle")
     cos_robot=math.cos(math.radians(robot.dir))
     sin_robot=math.sin(math.radians(robot.dir))
-    canvas.create_polygon(robot.x+robot.longueur/2*cos_robot-robot.largeur/2*sin_robot,
-                          robot.y+robot.longueur/2*sin_robot+robot.largeur/2*cos_robot,
-                          robot.x-robot.longueur/2*cos_robot-robot.largeur/2*sin_robot,
-                          robot.y-robot.longueur/2*sin_robot+robot.largeur/2*cos_robot,
-                          robot.x-robot.longueur/2*cos_robot+robot.largeur/2*sin_robot,
-                          robot.y-robot.longueur/2*sin_robot-robot.largeur/2*cos_robot,
-                          robot.x+robot.longueur/2*cos_robot+robot.largeur/2*sin_robot,
-                          robot.y+robot.longueur/2*sin_robot-robot.largeur/2*cos_robot,
+    canvas.create_polygon(robot.y+robot.longueur/2*cos_robot-robot.largeur/2*sin_robot,
+                          robot.x+robot.longueur/2*sin_robot+robot.largeur/2*cos_robot,
+                          robot.y-robot.longueur/2*cos_robot-robot.largeur/2*sin_robot,
+                          robot.x-robot.longueur/2*sin_robot+robot.largeur/2*cos_robot,
+                          robot.y-robot.longueur/2*cos_robot+robot.largeur/2*sin_robot,
+                          robot.x-robot.longueur/2*sin_robot-robot.largeur/2*cos_robot,
+                          robot.y+robot.longueur/2*cos_robot+robot.largeur/2*sin_robot,
+                          robot.x+robot.longueur/2*sin_robot-robot.largeur/2*cos_robot,
                           fill="blue",tags="rectangle")
-    canvas.create_line(robot.x+robot.longueur/2*cos_robot-robot.largeur/2*sin_robot,
-                          robot.y+robot.longueur/2*sin_robot+robot.largeur/2*cos_robot,
-                          robot.x+robot.longueur/2*cos_robot+robot.largeur/2*sin_robot,
-                          robot.y+robot.longueur/2*sin_robot-robot.largeur/2*cos_robot,
-                          fill="red",tags="rectangle")
 dessineRobot(cnv,robot1)
 
 
@@ -68,16 +60,19 @@ def move(event):
     global robot1,monde,vitesse
     key = event.keysym
     pas=speed(event)
+    if key =='p':
+        #robot1.avancer(5, monde)  # Utilisation de la méthode avancer avec le monde
+        robot1.mouvement(0.05)
     if key == 'Up':
         #robot1.avancer(5, monde)  # Utilisation de la méthode avancer avec le monde
-        monde.avancer_robot(pas,robot1)
+        robot1.avancer()
     elif key == 'Down':
         #robot1.avancer(-5, monde)   # Utilisation de la méthode reculer avec le monde
-        monde.avancer_robot(-pas,robot1)
-    elif key == 'Left':
-        robot1.tourner_droite(10)
+        robot1.reculer()
     elif key == 'Right':
-        robot1.tourner_gauche(10)
+        robot1.tourner_droite()
+    elif key == 'Left':
+        robot1.tourner_gauche()
 
     # Mise à jour des coordonnées du robot sur le canevas
     dessineRobot(cnv,robot1)
