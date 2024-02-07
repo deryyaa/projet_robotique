914ace554f157b2d24385a76ad88502d1e0c7e04
hugoamadoUPMC <hugoamado1007@gmail.com>
2024-02-07 10:44:09 +0100

    nouvelle version de robot avec vitesse des deux roues

--- futurama/dexter/robot.py
+++ futurama/dexter/robot.py
@@ -2,40 +2,46 @@ import sys
 import math
 import random
 import time 
-from futurama.univers.monde import Monde
-
-#mettre monde dans self
 
 class Robot:
-    def __init__(self, x, y, longueur, largeur, vitesse=0, dir=0):
-        self.vitesse = vitesse
-        self.vg=0 # Vitesse de la roue gauche
-        self.vd=0 # Vitesse de la roue droite
+    def __init__(self, x, y, longueur, largeur, vitesse_max, dir=0):
+        self.vitesse_max = vitesse_max
+        self.taille_roue = 10
+        self.vg=4 # Vitesse de la roue gauche
+        self.vd=2 # Vitesse de la roue droite
         self.nom="dexter"
         self.x = x
         self.y = y
         self.dir = dir % 360 # angle en degré
         self.largeur = largeur # largeur du robot en cm
         self.longueur = longueur # longueur du robot en cm
-        self.monde=Monde(20,20)
 
-    def avancer(self, distance):
+    def avancer(self):
         """ Avance le robot dans sa direction actuelle """
-        dx = distance * math.cos(math.radians(self.dir))
-        dy = distance * math.sin(math.radians(self.dir))
-        if self.monde.peut_avancer(dx, dy,self):
-            self.x += dx
-            self.y += dy
+        self.vd=self.vitesse_max
+        self.vg=self.vitesse_max
+        
+    def reculer(self):
+        """ Recule le robot dans sa direction opposée """
+        self.vd=-self.vitesse_max
+        self.vg=-self.vitesse_max
 
-
-    def tourner_droite(self, angle):
+    def tourner_droite(self):
         """ Tourne le robot vers la droite """
-        self.dir = (self.dir - angle) % 360
+        self.vd=-self.vitesse_max
+        self.vg=self.vitesse_max
 
-    def tourner_gauche(self, angle):
+    def tourner_gauche(self):
         """ Tourne le robot vers la gauche """
-        self.dir = (self.dir + angle) % 360
-
+        self.vd=self.vitesse_max
+        self.vg=-self.vitesse_max
+        
+    def mouvement(self, dt):
+        self.x += (self.taille_roue*(self.vg+self.vd)/2.0) * math.cos(math.radians(self.dir)) * dt
+        self.y -= (self.taille_roue*(self.vg+self.vd)/2.0) * math.sin(math.radians(self.dir)) * dt
+        self.dir += (self.vd - self.vg) * dt 
+    
+    
     def vitesse_discrete(self,distance,temps,monde):
         """Déplacer le robot avec une distance dans le monde pendant un temps """
         print("le robot commence a se deplacer.")
