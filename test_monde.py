47b1c3c787d18d9798f40016d001503d342c4ea2
deryyaa <derya-yaya@outlook.fr>
2024-02-10 13:12:17 +0100

    teste utilitaire monde

--- test_monde.py
+++ test_monde.py
@@ -17,8 +17,14 @@ class Test_Monde(unittest.TestCase):
         self.robot.avancer_(8,self.monde)
         self.assertFalse(self.monde.peut_avancer(1,1,self.robot))
 
-
-
+    def test_avancer_robot(self):
+         self.monde.avancer_robot(6,self.robot)
+         x=int(self.robot.x)
+         y=self.robot.y
+         print(self.robot.x)
+         print(self.obstacle.x)
+         self.assertEqual(x,self.obstacle.x)
+         
 
 
 
