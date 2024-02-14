8e1d42078712479a6e7ad97dcda387458f080700
hugoamadoUPMC <hugoamado1007@gmail.com>
2024-02-14 08:59:18 +0100

    modification physique pour ca particulier du robot qui ne bougeait pas

--- futurama/dexter/robot.py
+++ futurama/dexter/robot.py
@@ -76,7 +76,11 @@ class Robot:
         self.x += (self.taille_roue*(self.vg+self.vd)/2.0) * math.cos(self.dir) * dt
         self.y -= (self.taille_roue*(self.vg+self.vd)/2.0) * math.sin(self.dir) * dt
         if(self.vg!=self.vd):
-            if(self.vg>self.vd):
+            if(self.vg==0):
+                self.dir+=self.vd*dt/(-self.d*self.vd*dt/(self.vg*dt-self.vd*dt))
+            elif(self.vd==0):
+                self.dir-=self.vg*dt/(-self.d*self.vg*dt/(self.vd*dt-self.vg*dt))
+            elif(self.vg>self.vd):
                 self.dir-=self.vg*dt/(-self.d*self.vg*dt/(self.vd*dt-self.vg*dt))
             else:
                 self.dir+=self.vd*dt/(-self.d*self.vd*dt/(self.vg*dt-self.vd*dt))
