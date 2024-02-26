68a7359ddfb117d21c7c24d280b188411c9a1d5b
Rasheequa BAGADAD SAIB <rasheequa.m@gmail.com>
2024-02-26 12:15:48 +0100

    ajout step() start() stop() dans les classes du controleurs (pas fini)

--- src/univers/controleur.py
+++ src/univers/controleur.py
@@ -1,41 +1,52 @@
 import time
 class Controleur:
     class AvancerToutDroit:
-        def __init__(self, robot, FPS = 100):
+        def __init__(self, robot, distance, FPS = 100):
             self.robot = robot
+            self.distance = distance
             self.FPS = FPS
 
-        def avancer_tout_droit(self, distance):
+        def start(self):
+            self.parcouru = 0
+
+        def stop(self):
+            return self.parcouru>self.distance
+        
+        def step(self):
             self.robot.vg = 10
             self.robot.vd = 10
-            distance_parcouru = 0
-            while distance_parcouru<distance :
-                if (self.robot.capteur_distance()<1):
-                    self.robot.move(1./self.FPS)
-                    distance_parcouru+=self.robot.vg*(1./self.FPS)
+            self.parcouru += self.robot.vg*(1./self.FPS)
+            if self.stop(): return
+            self.robot.move(1./self.FPS)
+
 
     class Tourner:
-        def __init__(self, robot):
+        def __init__(self, robot, distance, FPS = 100):
             self.robot = robot
+            self.distance = distance
+            self.FPS=FPS
 
-        def tourner(self, degre):
+        def start(self):
+            self.parcouru = 0
+
+        def step(self):
             self.robot.vg = 10
             self.robot.vd = -10
-            distance_parcouru = 0
-            while distance_parcouru<degre :
-                if (self.robot.capteur_distance()<1):
-                    self.robot.move(1./self.FPS)
-                    distance_parcouru+=self.robot.vg*(1./self.FPS)
+            self.parcouru += self.robot.vg*(1./self.FPS)
+            if self.stop(): return
+            self.robot.move(1./self.FPS)
+            
+            #if (self.robot.capteur_distance()<1):
+      
+        def stop(self):
+            return self.parcouru>self.distance
 
         
     class TracerCarre:
         def __init__(self, robot):
             self.robot = robot
 
-        def tracer_carre(self, distance):
-
-            for _ in range(4):
-                Controleur.AvancerToutDroit.avancer_tout_droit(distance)
-                Controleur.Tourner.tourner()
-
-    
+        def start(self):
+            
+            Controleur.AvancerToutDroit.avancer_tout_droit(dist=10)
+            
