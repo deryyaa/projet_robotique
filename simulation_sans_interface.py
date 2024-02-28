dfa33fa066912939d9583bd0b81f441be54c9dc9
deryyaa <derya-yaya@outlook.fr>
2024-02-28 20:37:07 +0100

    suppression de commentaire

--- simulation_sans_interface.py
+++ simulation_sans_interface.py
@@ -1,7 +1,6 @@
 from src.univers.robot import Robot
 from src.univers.monde import Monde
 from src.univers.obstacle import Obstacle
-#from src.graphique.graphique import Graphique
 from src.controleur.strategie import *
 import math
 import time
@@ -16,30 +15,16 @@ robot = Robot(300, 200, 20, 15 , 10)  # Position du robot dans le monde
 # Création du monde
 monde = Monde(500, 500,robot)
 
-
-
-
-#Paramétrage graphique
-#fenetre = Tk()
-#fenetre.title("Robot dans le monde")
-#cnv = Canvas(fenetre, width=monde.ligne, height=monde.colonne, bg="ivory")
-#cnv.pack()
-#graph=Graphique(monde,cnv)
-
-
 lock=threading.Lock()
-
 
 def update():
     while True:
         monde.update()
-        #graph.update()
         time.sleep(1./FPS)
-        #fenetre.update()
+        
 
 
 def runAvancer(FPS):
-    #graph.dessineObstacle()
     strategie=AvancerToutDroit(100,robot)
     threading.Thread(target=update).start()
     strategie.start()
@@ -48,7 +33,7 @@ def runAvancer(FPS):
         time.sleep(1./FPS)
 
 def runTourner(FPS):
-    #graph.dessineObstacle()
+
     strategie=Tourner(math.pi,robot)
     threading.Thread(target=update).start()
     strategie.start()
@@ -58,4 +43,3 @@ def runTourner(FPS):
 
 threading.Thread(target=runAvancer, args=(100,)).start()
 threading.Thread(target=runTourner, args=(100,)).start()
-#fenetre.mainloop()
