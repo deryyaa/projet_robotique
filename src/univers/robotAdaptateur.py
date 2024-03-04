0fd85c9cb973cfdc23de7062817cec6c72c52342
deryyaa <derya-yaya@outlook.fr>
2024-03-04 11:40:58 +0100

    création de robotAdaptateur

---
+++ src/univers/robotAdaptateur.py
@@ -0,0 +1,5 @@
+from robot import Robot
+
+class RobotAdaptateur(Robot):
+    def __init__(self, x, y, longueur, largeur, vitesse_max,dir=0):
+        Robot.__init__(self, x, y, longueur, largeur, vitesse_max,dir)
