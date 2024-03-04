c42c4890dc2fed0805e6d87723504b869d672f37
deryyaa <derya-yaya@outlook.fr>
2024-03-04 22:24:15 +0100

    création d'adaptateur pour le robot21013

--- src/univers/robotAdaptateur.py
+++ src/univers/robotAdaptateur.py
@@ -1,7 +1,11 @@
 from robot import Robot
 
 class RobotAdaptateur(Robot):
-    def __init__(self, x, y, longueur, largeur, vitesse_max,dir=0):
+    def __init__(self, x, y, longueur, largeur, vitesse_max, dir=0):
         Robot.__init__(self, x, y, longueur, largeur, vitesse_max,dir)
+    
 
-
+    def setVitesseRoue(self,vrg,vrd):
+        Robot.vg=vrg
+        Robot.vd=vrd
+    
