0bc37a8a5bca2cac7651bc7e174d3640a2c02db9
hugoamadoUPMC <hugoamado1007@gmail.com>
2024-03-06 21:17:12 +0100

    cleaning et implementation de capteur distance

--- simulation.py
+++ simulation.py
@@ -11,7 +11,7 @@ from tkinter import *
 FPS=100
 
 #Création de robot
-robot = Robot(300, 460, 20, 15 , 10 , math.pi/2)  # Position du robot dans le monde
+robot = Robot(300, 200, 20, 15 , 10 )  # Position du robot dans le monde
 
 # Création du monde
 monde = Monde(500, 500, robot)
@@ -30,7 +30,6 @@ def update():
     while True:
         monde.update()
         graph.update()
-        robot.capteur_distance(monde)
         time.sleep(1./FPS)
 
 
@@ -45,7 +44,7 @@ def run(strat,FPS):
         time.sleep(1./FPS)
 
 #threading.Thread(target=run, args=(TracerCarre(50,robot),100,)).start()
-threading.Thread(target=run, args=(AvancerToutDroit(50,robot),100,)).start()
+threading.Thread(target=run, args=(AvancerToutDroit(50,robot,monde),100,)).start()
 #threading.Thread(target=run, args=(Tourner(50,robot),100,)).start()
 
 fenetre.mainloop()
