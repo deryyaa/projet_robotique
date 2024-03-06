6c0ef7fcadf4fb6957f0757ee1012c512b48b5c9
deryyaa <derya-yaya@outlook.fr>
2024-03-06 11:38:33 +0100

    ajout de move dans l'adaptateur

--- src/univers/adaptateur.py
+++ src/univers/adaptateur.py
@@ -3,11 +3,27 @@ from robot2I013 import Robot2IN013
 class Robot2I013Adaptateur():
     def __init__(robot):
         self.robot=robot
+        self.x=0
+        self.y=0
+        
+    def move(self,dt):
+        x=self.x
+        y=self.y
+        self.x += ((self.vg*dt+self.vd*dt)/2.0) * math.cos(self.dir)
+        self.y += ((self.vg*dt+self.vd*dt)/2.0) * math.sin(self.dir)
+        if(self.vg!=self.vd):
+            if(abs(self.vg)>abs(self.vd)):
+                self.dir-=self.vg*dt/(-self.d*self.vg*dt/(self.vd*dt-self.vg*dt))
+            else:
+                self.dir+=self.vd*dt/(-self.d*self.vd*dt/(self.vg*dt-self.vd*dt))
+        self.distanceParcouru+=math.sqrt((self.x-x)**2+(self.y-y)**2)
 
-    
+        
     def setVitesse(self, vg,vd):
-        self.robot.set_motor_dps(self.robot.MOTOR_LETF,vg)
-        self.robot.set_motor_dps(self.robot.MOTOR_RIGHT,vd)
+        self.vg=self.robot.set_motor_dps(self.robot.MOTOR_LETF,vg)
+        self.vd=self.robot.set_motor_dps(self.robot.MOTOR_RIGHT,vd)
 
     def getPosition():
-        return self.robot.get_motor_position()
+        return (self.x,self.y)
+    
+    def getDistanceParcouru(self):
