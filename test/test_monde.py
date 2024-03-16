41013b69eb208c0bcbc6194c015d15635f4aae5a
deryyaa <derya-yaya@outlook.fr>
2024-03-16 16:08:37 +0100

    test unitaite monde

--- test/test_monde.py
+++ test/test_monde.py
@@ -19,7 +19,13 @@ class Test_Monde(unittest.TestCase):
     def test_pas_de_collision(self):
         self.assertFalse(self.monde.detecter_collision(7,4))
         self.assertFalse(self.monde.detecter_collision(7,4))
-         
+    
+    def test_collision_rect():
+        rectangle_obstacle = [(10, 20), (30, 40), (30, 40)]  # Coordonnées et dimensions du rectangle obstacle
+        rectangle_robot = [(25, 35), (45, 55), (45, 55)]  # Coordonnées et dimensions du rectangle robot
+
+        resultat_collision = collision_rect(rectangle_obstacle, rectangle_robot)
+        print(f"Collision détectée : {resultat_collision}")
 
 if __name__ =='__main__':
     unittest.main()
