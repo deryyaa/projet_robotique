4932f4cf550579baacfc47aba49f14e637912227
deryyaa <derya-yaya@outlook.fr>
2024-03-18 11:34:28 +0100

    uniteste de test_collision

--- test/test_monde.py
+++ test/test_monde.py
@@ -1,7 +1,7 @@
 import unittest
 import math
 from src.univers.robot import Robot
-from src.univers.monde import Monde
+from src.univers.monde import collision_rect, Monde
 from src.univers.obstacle import Obstacle
 
 #python3 -m unittest test/test_monde.py -v
@@ -13,19 +13,12 @@ class Test_Monde(unittest.TestCase):
         self.monde.creation_obstacle(15,15,1,1)
         print(self.monde.obstacles)
 
-    def test_detecter_collision(self):
-        self.assertTrue(self.monde.detecter_collision(15,15))
 
-    def test_pas_de_collision(self):
-        self.assertFalse(self.monde.detecter_collision(7,4))
-        self.assertFalse(self.monde.detecter_collision(7,4))
-    
-    def test_collision_rect():
-        rectangle_obstacle = [(10, 20), (30, 40), (30, 40)]  # Coordonnées et dimensions du rectangle obstacle
-        rectangle_robot = [(25, 35), (45, 55), (45, 55)]  # Coordonnées et dimensions du rectangle robot
-
-        resultat_collision = collision_rect(rectangle_obstacle, rectangle_robot)
-        print(f"Collision détectée : {resultat_collision}")
+    def test_collision_rect(self):
+        rectangle_obstacle = [(10, 20), (30, 40), (30, 40),(30,40)]  
+        rectangle_robot = [(10, 20), (30, 40), (30, 40),(30,40)]  
+        self.assertTrue(collision_rect(rectangle_obstacle,rectangle_robot))
+        
 
 if __name__ =='__main__':
     unittest.main()
