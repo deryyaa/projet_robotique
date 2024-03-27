7c2f61c22669e375cd81cc8328e72c312bb6f4af
deryyaa <derya-yaya@outlook.fr>
2024-03-27 10:29:20 +0100

    modification de move dans l'adaptateur

--- src/controleur/adaptateur.py
+++ src/controleur/adaptateur.py
@@ -20,23 +20,27 @@ class Robot2I013Adaptateur():
         self.taille_roue = 8
         self.angle_parcourue=0
         self.d=largeur
-        self.last_time=0
+        self.last_time=time.time()
 
     def move(self,dt):  #update, mise a jours des stats
 
         current_time = time.time()  # Obtient le temps actuel
         dt = current_time - self.last_time  # Calcule la différence de temps
         self.last_time = current_time  # Met à jour le temps de la dernière mise à jour
+
+        print(self.robot.get_motor_position(),type(self.robot.get_motor_position()))
         rg,rd=self.robot.get_motor_position()
         old_x=self.x
         old_y=self.y #vitess roue droite et droite
-        self.x += ((rg*self.robot.WHEEL_DIAMETER*dt+rd*self.robot.WHEEL_DIAMETER*dt)/2.0) * math.cos(self.dir)
-        self.y += ((rg*self.robot.WHEEL_DIAMETER*dt+rd*self.robot.WHEEL_DIAMETER*dt)/2.0) * math.sin(self.dir)
+        vg=rg*self.robot.WHEEL_DIAMETER/2
+        vd=rd*self.robot.WHEEL_DIAMETER/2
+        self.x += ((vg*dt+vd*dt)/2.0) * math.cos(self.dir)
+        self.y += ((vg*dt+vd*dt)/2.0) * math.sin(self.dir)
         if(self._vg!=self._vd):
-            if(abs(self._vg)>abs(self._vd)):
-                self.dir-=self._vg*dt/(-self.d*self._vg*dt/(self._vd*dt-self._vg*dt))
+            if(abs(vg)>abs(vd)):
+                self.dir-=vg*dt/(-self.d*vg*dt/(vd*dt-vg*dt))
             else:
-                self.dir+=self._vd*dt/(-self.d*self._vd*dt/(self._vg*dt-self._vd*dt))
+                self.dir+=vd*dt/(-self.d*vd*dt/(vg*dt-vd*dt))
         self.distanceParcouru+=math.sqrt((self.x-old_x)**2+(self.y-old_y)**2)
         self.angle_parcourue=math.atan2(old_y-self.y,old_x-self.x) #angle entre deux points
         
@@ -58,13 +62,16 @@ class Robot2I013Adaptateur():
         distanceP_capteur = 0
         capteur_x = self.x+self.longueur
         capteur_y = self.y+self.largeur
+        rg,rd=self.robot.get_motor_position()
+        vg=rg*self.robot.WHEEL_DIAMETER/2
+        vd=rd*self.robot.WHEEL_DIAMETER/2
 
         while not monde.detecter_collision(capteur_x, capteur_y): #tant qu'il n'a rien detecté, on fait avancer le capteur dans la direction de robot et on incremente sa distance parcourue
             distanceP_capteur+= 1
             if distanceP_capteur>50:
                 return distanceP_capteur
-            capteur_x += ((self._vg*0.01+self._vd*0.01)/2.0) * math.cos(self.dir)
-            capteur_y += ((self._vg*0.01+self._vd*0.01)/2.0) * math.sin(self.dir)
+            capteur_x += ((vg*0.01+vd*0.01)/2.0) * math.cos(self.dir)
+            capteur_y += ((vg*0.01+vd*0.01)/2.0) * math.sin(self.dir)
             
         print(f"Position actuelle du robot : {[self.x, self.y]}, Distance jusqu'a l'obstacle : {distanceP_capteur}")
         return distanceP_capteur    
