5177b11cd77c7b3fe90d69d9f6076783510b7457
Rasheequa BAGADAD SAIB <rasheequa.m@gmail.com>
2024-04-24 09:38:28 +0200

    modif forme robot

--- graphique3D.py
+++ graphique3D.py
@@ -12,15 +12,15 @@ import math
 import time
 import threading
 
-#Création de robot
-robot = Robot.creation_robot() 
+# Création du robot
+robot = Robot.creation_robot()
 
 # Création du monde
 monde = Monde.creation_monde(robot)
-robot.monde=monde
+robot.monde = monde
 monde.place_obstacle()
 
-class graphique3D:
+class Graphique3D:
     def __init__(self, master):
         self.master = master
         self.master.title("Simulation 3D")
@@ -37,26 +37,60 @@ class graphique3D:
         # Lancement de l'animation
         self.animation()
 
+    def draw_robot(self):
+        # Coordonnées du robot
+        x, y, z = robot.x, robot.y, robot.z
+        longueur, largeur, hauteur = robot.longueur, robot.largeur, robot.hauteur
+
+        # Dessine les lignes pour former le cube
+        edges = [
+            [x - longueur / 2, y - largeur / 2, z - hauteur / 2],
+            [x + longueur / 2, y - largeur / 2, z - hauteur / 2],
+            [x + longueur / 2, y + largeur / 2, z - hauteur / 2],
+            [x - longueur / 2, y + largeur / 2, z - hauteur / 2],
+            [x - longueur / 2, y - largeur / 2, z + hauteur / 2],
+            [x + longueur / 2, y - largeur / 2, z + hauteur / 2],
+            [x + longueur / 2, y + largeur / 2, z + hauteur / 2],
+            [x - longueur / 2, y + largeur / 2, z + hauteur / 2]
+        ]
+
+        # Liste des arêtes qui forment le cube
+        edges = [
+            [edges[0], edges[1], edges[2], edges[3], edges[0]],
+            [edges[4], edges[5], edges[6], edges[7], edges[4]],
+            [edges[0], edges[4]],
+            [edges[1], edges[5]],
+            [edges[2], edges[6]],
+            [edges[3], edges[7]]
+        ]
+
+        for edge in edges:
+            x, y, z = zip(*edge)
+            self.ax.plot(x, y, z, color='red')
+
     def animation(self):
         # Supprime les graphiques précédents
         self.ax.clear()
 
-        # Crée des données pour la simulation 3D
+        # Trace la trajectoire en 3D
         t = np.linspace(0, 10, 100)
         x = np.sin(t)
         y = np.cos(t)
         z = t
-
-        # Trace la trajectoire en 3D
         self.ax.plot(x, y, z)
 
         # Ajout du robot dans le graphique 3D
-        self.ax.scatter(robot.x, robot.y, robot.z, color='red', marker='o', label='Robot')
+        self.draw_robot()
 
         # Définit les étiquettes des axes
         self.ax.set_xlabel('X')
         self.ax.set_ylabel('Y')
         self.ax.set_zlabel('Z')
+
+        # Définit l'origine du repère à (0,0,0)
+        self.ax.set_xlim([-monde.ligne, monde.ligne])
+        self.ax.set_ylim([-monde.colonne, monde.colonne])
+        self.ax.set_zlim([0, monde.colonne])
 
         # Met à jour le graphique
         self.canvas.draw()
@@ -66,7 +100,7 @@ class graphique3D:
 
 def main():
     root = tk.Tk()
-    app = graphique3D(root)
+    app = Graphique3D(root)
     root.mainloop()
 
 if __name__ == "__main__":
