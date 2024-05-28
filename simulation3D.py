d5f7e1d18f17b7cb9adaba394e52416a98a624f0
deryyaa <derya-yaya@outlook.fr>
2024-05-28 16:55:37 +0200

    changement dans simulation

--- simulation3D.py
+++ simulation3D.py
@@ -7,43 +7,11 @@ import time
 import threading
 from tkinter import *
 from src.graphique.graphique3D import Graphique3D
+from direct.showbase.ShowBase import ShowBase
 
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
 
-FPS = 100
-
-# Création de robot
-robot = Robot.creation_robot()
-
-# Création du monde
-monde = Monde.creation_monde(robot)
-robot.monde = monde
-monde.place_obstacle()
-
-# Création de l'instance Graphique3D
-root = Tk()
-graph = Graphique3D(monde, root)
-
-# Paramétrage graphique
-def update():
-    graph.dessineObstacle()
-    while True:
-        monde.update()
-        graph.update()
-        time.sleep(1. / FPS)
-
-def run(strat):
-    condition = True
-    threading.Thread(target=update).start()
-    strat.start()
-    while condition:
-        strat.step()
-        if strat.stop() or robot.crash:
-            print(robot.capteur_distance())
-            robot.setVitesse(0, 0)
-            condition = False
-        time.sleep(1. / FPS)
-
-# Lancement de la stratégie
-threading.Thread(target=run, args=(TracerCarre(50, robot),)).start()
-
-root.mainloop()
+app = MyApp()
+app.run()
