e5c40fcf40a5347d06a84c3f95ca6260b145c240
deryyaa <derya-yaya@outlook.fr>
2024-05-28 16:59:58 +0200

    modif

--- src/graphique/graphique3D.py
+++ src/graphique/graphique3D.py
@@ -1,12 +1,7 @@
-
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
 from src.univers.robot import Robot
 from src.univers.monde import Monde
 from src.univers.obstacle import Obstacle
-import math
+
 
 
 from direct.showbase.ShowBase import ShowBase
