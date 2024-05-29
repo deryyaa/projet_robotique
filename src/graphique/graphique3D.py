b167f2ffb2b2384b641a32eaf9b46d660b95f2a2
Rasheequa BAGADAD SAIB <rasheequa.m@gmail.com>
2024-05-29 16:07:22 +0200

    3D update

--- src/graphique/graphique3D.py
+++ src/graphique/graphique3D.py
@@ -1,14 +1,43 @@
-from src.univers.robot import Robot
-from src.univers.monde import Monde
-from src.univers.obstacle import Obstacle
-
-
-
+from panda3d.core import loadPrcFileData
 from direct.showbase.ShowBase import ShowBase
+from direct.actor.Actor import Actor
+
+confVars = """
+win-size 1280 720
+window-title My Robot
+show-frame-rate-meter True
+"""
+
+loadPrcFileData("", confVars)
 
 class MyRobot(ShowBase):
     def __init__(self):
-        ShowBase.__init__(self)
+        super().__init__()
 
-app = MyRobot()
-app.run()
+        # Charger le modèle de l'objet
+        self.robot = Actor("model/robot", {"anim1": "model/robot"})
+        self.robot.setPos(0, 10, 0)
+        self.robot.reparentTo(self.render)
+
+        # Charger le modèle de l'environnement
+        env = self.loader.loadModel("model/area")
+        env.setPos(0, 0, 0)  # Ajusté pour que l'environnement soit à la position d'origine
+        env.reparentTo(self.render)
+
+        # Placer la caméra au-dessus de l'objet principal
+        self.camera.setPos(0, -20, 20)  # Ajusté pour observer le robot
+        self.camera.lookAt(self.robot)
+
+        self.x = 0
+        self.y = 0
+
+        self.taskMgr.add(self.update, "update")
+
+    def update(self, task):
+        self.robot.setPos(self.x, self.y, 0)
+        self.x += 0.01  
+        self.y += 0.01  
+        return task.cont  # Continuer l'appel de cette tâche à chaque frame
+
+game = MyRobot()
+game.run()
