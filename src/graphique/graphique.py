36865ad577f8b0251f133b4463352f5fadbab1fe
Rasheequa BAGADAD SAIB <rasheequa.m@gmail.com>
2024-06-02 22:04:43 +0200

    ajout docstrings, commentaire

--- src/graphique/graphique.py
+++ src/graphique/graphique.py
@@ -7,66 +7,140 @@ import time
 from threading import Thread
 
 class Graphique(Thread):
+    """
+    Cette classe gère l'affichage graphique du monde et du robot sur une interface Tkinter.
+    Hérite de Thread pour permettre l'exécution en parallèle avec d'autres processus.
 
+    """
     def __init__(self,monde,canvas,fenetre):
-        self.monde=monde
-        self.fenetre=fenetre
-        self.cnv=canvas
-        self.dessineObstacle()
-        self.dessineRobot()
-        
+        """
+        Initialise l'objet Graphique avec le monde, le canvas et la fenêtre donnés.
+        Dessine initialement les obstacles et le robot sur le canvas, et configure le label des coordonnées.
+        """
+        self.monde=monde # Attribue le monde 
+        self.fenetre=fenetre # Attribue une fenetre graphique, l'interface où les éléments seront affichés
+        self.cnv=canvas # Attribue un canvas, une toile sur laquelle on peut dessiner des éléments graphiques
+        self.dessineObstacle() # Appelle la méthode pour dessiner les obstacles sur le canvas
+        self.dessineRobot() # Appelle la méthode pour dessiner le robot sur le canvas
+        self.coord_label = Label(fenetre, text="")  
+        # Crée un widget Label dans la fenêtre (fenetre) pour afficher du texte, initialement vide
+        self.coord_label.pack()  
+        # Ajoute le label à la fenêtre graphique et l'affiche à l'écran
+
+
     def coordonneesRobot(self, robot):
         """Calcule les coordonnees du robot en fonction de son angle de rotation"""
+        # Calcul des valeurs du cosinus et du sinus de l'angle de rotation du robot
         cos_dir = math.cos(robot.dir)
         sin_dir = math.sin(robot.dir)
 
         # Coordonnées du robot
         points = [
+            # Calcul des coordonnées du coin avant-droit du robot
             (robot.x + robot.largeur / 2 * sin_dir - robot.longueur / 2 * cos_dir, self.monde.colonne - robot.y + robot.largeur / 2 * cos_dir + robot.longueur / 2 * sin_dir),
+
+            # Calcul des coordonnées du coin arrière-droit du robot
             (robot.x - robot.largeur / 2 * sin_dir - robot.longueur / 2 * cos_dir, self.monde.colonne - robot.y - robot.largeur / 2 * cos_dir + robot.longueur / 2 * sin_dir),
+            
+            # Calcul des coordonnées du coin arrière-gauche du robot
             (robot.x - robot.largeur / 2 * sin_dir + robot.longueur / 2 * cos_dir, self.monde.colonne - robot.y - robot.largeur / 2 * cos_dir - robot.longueur / 2 * sin_dir),
+            
+            # Calcul des coordonnées du coin avant-gauche du robot
             (robot.x + robot.largeur / 2 * sin_dir + robot.longueur / 2 * cos_dir, self.monde.colonne - robot.y + robot.largeur / 2 * cos_dir - robot.longueur / 2 * sin_dir)
         ]
 
-        return points
+        return points # Retourne une liste de tuples, chaque tuple représente les coordonnées (x, y)
 
     def dessineRobot(self):
         """Calcule et dessine le robot sur la fenetre"""
+        # Récupération de l'objet robot depuis l'attribut monde
         robot = self.monde.robot
+        
+        # Calcul des coordonnées des coins du robot
         points = self.coordonneesRobot(robot)
 
+        # Dessin du corps du robot
+        # *points décompose la liste des coordonnées en arguments individuels
+        # fill="blue" définit la couleur de remplissage du polygone
+        # outline="black" définit la couleur du contour du polygone
+        # tags="rectangle" permet d'associer un tag "rectangle" à cet élément graphique, utile pour manipuler l'élément plus tard
         self.rectangle = self.cnv.create_polygon(*points, fill="blue", outline="black", tags="rectangle") # corps du robot
+        
+        # Dessin de la tête du robot
+        # points[2] et points[3] sont les coordonnées des coins arrière-gauche et avant-gauche du robot
+        # fill="red" définit la couleur de la ligne
+        # width=2 définit l'épaisseur de la ligne
+        # tags="head" permet d'associer un tag "head" à cet élément graphique, utile pour manipuler l'élément plus tard
         self.head = self.cnv.create_line(points[2], points[3], fill="red", width=2, tags="head") # tete du robot
 
     def deplaceRobot(self):
         """Deplace le robot dans la fenetre"""
+        # Récupération de l'objet robot depuis l'attribut monde
         robot = self.monde.robot
+        
+        # Calcul des coordonnées des coins du robot
         points = self.coordonneesRobot(robot)
 
-        # déplacement du robot
+        # Déplacement du corps du robot
+        # La méthode coords de canvas est utilisée pour mettre à jour les coordonnées d'un polygone existant
+        # *sum(points, ()) aplatit la liste des tuples de coordonnées en une liste simple
         self.cnv.coords(self.rectangle, *sum(points, ())) 
+        
+        # Déplacement de la tête du robot
+        # La méthode coords de canvas est utilisée pour mettre à jour les coordonnées d'une ligne existante
+        # points[2][0], points[2][1], points[3][0], points[3][1] sont les nouvelles coordonnées de la ligne
         self.cnv.coords(self.head, points[2][0], points[2][1], points[3][0], points[3][1]) 
         
     def dessineObstacle(self):
         """Ajoute les obstacles dans la simulation."""
+        # Parcours de la liste des obstacles dans le monde
         for obs in self.monde.obstacles:
+            # Création d'un rectangle pour représenter l'obstacle
+            # Les coordonnées du rectangle sont calculées en fonction de la position et des dimensions de l'obstacle
+            # Le rectangle est rempli avec la couleur "grey"
             self.cnv.create_rectangle(obs.x-obs.longueur/2,self.monde.colonne-obs.y-obs.largeur/2,
                                       obs.x+obs.longueur/2,self.monde.colonne-obs.y+obs.largeur/2,
                                       fill="grey")
+            # Création du texte pour afficher les coordonnées de l'obstacle
+            coord_text = f"({obs.x}, {obs.y})"
+            self.cnv.create_text(
+                obs.x,  # Position x du texte
+                self.monde.colonne - obs.y,  # Position y du texte (inversée)
+                text=coord_text,  # Texte à afficher
+                anchor=NW,  # Ancrage du texte au coin supérieur gauche
+                fill="black"  # Couleur du texte en noir
+            )
 
     def update(self):
         """Met à jour l'affichage."""
+         # Déplace le robot selon sa nouvelle position
+        # Cette ligne appelle la méthode deplaceRobot pour mettre à jour la position graphique du robot sur le canvas
         self.deplaceRobot()
+        
+        # Dessine le trait représentant le chemin parcouru par le robot
+        # Cette ligne appelle la méthode dessineTrait pour tracer le chemin que le robot a parcouru
         self.dessineTrait()
+        
+        # Met à jour l'affichage de la fenêtre
+        # Cette ligne force une mise à jour de l'affichage de la fenêtre pour refléter les changements
         self.fenetre.update()
+        
+        # Met à jour le label des coordonnées du robot
+        # Cette ligne appelle la méthode updateCoordLabel pour mettre à jour le texte affiché des coordonnées du robot
+        self.updateCoordLabel()
 
     def dessineTrait (self):
         """Dessine un trait en fonction du chemin du robot"""
+        # Récupération de l'objet robot depuis l'attribut monde
         robot=self.monde.robot
+
+        # Vérification de l'existence des coordonnées précédentes du robot
         if not hasattr(self, 'prev_x') or not hasattr(self, 'prev_y'):
+            # Si les coordonnées précédentes n'existent pas, les initialiser à None
             self.prev_x = None
             self.prev_y = None
 
+        # Si les coordonnées précédentes existent et ne sont pas None
         if self.prev_x is not None and self.prev_y is not None:
             # Coordonnées du début et de la fin du trait
             x1 = self.prev_x
@@ -80,3 +154,15 @@ class Graphique(Thread):
         # Mettre à jour les coordonnées précédentes du robot
         self.prev_x = robot.x
         self.prev_y = robot.y
+
+    def updateCoordLabel(self):
+        """Met à jour le label des coordonnées du robot"""
+        # Récupération de l'objet robot depuis l'attribut monde
+        robot = self.monde.robot
+
+        # Création d'un texte formaté avec les coordonnées actuelles du robot
+        coord_text = f"coordonnées robot : x = {robot.x:.2f}, y = {robot.y:.2f}"
+
+        # Mise à jour du texte du label avec les nouvelles coordonnées du robot
+        # La méthode config est utilisée pour changer la configuration du label, ici on change le texte affiché
+        self.coord_label.config(text=coord_text)
--- src/graphique/graphique3D.py
+++ src/graphique/graphique3D.py
@@ -19,62 +19,77 @@ sync-video 0
 loadPrcFileData("", confVars)
 
 class MyRobot(ShowBase, DirectObject):
+    """Classe principale pour la visualisation 3D du robot dans Panda3D."""
+
     def __init__(self, monde, strategie):
+        """Initialise la classe MyRobot."""
+
         super().__init__()
 
-        self.monde = monde
+        self.monde = monde # Attribue un monde à la simulation
         self.robot = None  # Initialisation de l'acteur robot
-        self.x = monde.robot.x
-        self.y = monde.robot.y
-        self.z = monde.robot.z
-        self.direction = monde.robot.dir
-        self.speed = monde.robot.vitesse_max
-        self.strategie = strategie
 
-        self.camDist = 1000  # Distance of the camera from the robot
-        self.camAngle = 0  # Horizontal angle for rotating the camera around the robot
-        self.camPitch = 10  # Vertical angle for rotating the camera around the robot (set to -45 to look from above)
-        self.camHeight = 50  # Height of the camera above the robot
+        self.x = monde.robot.x  # Coordonnée x initiale du robot
+        self.y = monde.robot.y  # Coordonnée y initiale du robot
+        self.z = monde.robot.z  # Coordonnée z initiale du robot
+        self.direction = monde.robot.dir  # Direction initiale du robot
 
-        self.afficheEnvironnement()
-        self.afficheRobot()
-        self.afficheObstacle()
+        self.strategie = strategie  # Stratégie associée au robot
 
+        # Paramètres de la caméra
+        self.camDist = 1000  # Distance de la caméra par rapport au robot
+        self.camAngle = 0  # Angle horizontal pour la rotation de la caméra autour du robot
+        self.camPitch = 10  # Angle vertical pour la rotation de la caméra autour du robot (mis à -45 pour une vue de dessus)
+        self.camHeight = 50  # Hauteur de la caméra au-dessus du robot
+
+        # Affichage initial de l'environnement, du robot et des obstacles
+        self.afficheEnvironnement()  # Affiche l'environnement
+        self.afficheRobot()  # Affiche le robot
+        self.afficheObstacle()  # Affiche les obstacles
+
+        # Ajoute la méthode update3D à la gestion des tâches pour mettre à jour l'affichage
         self.taskMgr.add(self.update3D, "update")
 
-        # Enable mouse control for camera
-        self.disableMouse()
-        self.accept("mouse3", self.startCameraControl)
-        self.accept("mouse3-up", self.stopCameraControl)
-        self.accept("wheel_up", self.zoomIn)
-        self.accept("wheel_down", self.zoomOut)
-        self.cameraControl = False
+        # Active le contrôle de la caméra avec la souris
+        self.disableMouse()  # Désactive le contrôle de la caméra par défaut avec la souris
+        self.accept("mouse3", self.startCameraControl)  # Active le contrôle de la caméra avec le clic droit de la souris
+        self.accept("mouse3-up", self.stopCameraControl)  # Désactive le contrôle de la caméra lorsque le clic droit est relâché
+        self.accept("wheel_up", self.zoomIn)  # Zoom avant avec la molette de la souris
+        self.accept("wheel_down", self.zoomOut)  # Zoom arrière avec la molette de la souris
+        self.cameraControl = False  # Initialise le contrôle de la caméra à False
 
-        # Set initial camera position
+        # Définit la position initiale de la caméra
         self.setInitialCameraPosition()
 
-        # Setup text for displaying robot coordinates
+        # Configure le texte pour afficher les coordonnées du robot
         self.setupText()
 
+
     def startCameraControl(self):
-        self.cameraControl = True
-        self.mouseX = self.mouseWatcherNode.getMouseX()
+        """Démarre le contrôle de la caméra avec les boutons de la souris."""
+        
+        self.cameraControl = True  # Active le contrôle de la caméra
+        # Enregistre la position actuelle de la souris pour suivre les mouvements ultérieurs
+        self.mouseX = self.mouseWatcherNode.getMouseX()  
         self.mouseY = self.mouseWatcherNode.getMouseY()
-
+        
     def stopCameraControl(self):
-        self.cameraControl = False
-
-    def deplaceRobot(self):
-        self.x = self.robot.x
-        self.y = self.robot.y
+        """Arrête le contrôle de la caméra avec le bouton de la souris."""
+        self.cameraControl = False # Désactive le contrôle de la caméra
         
     def zoomIn(self):
-        self.camDist = max(20, self.camDist - 50)
+        """Rapproche la caméra du robot."""
+        # Réduit la distance de la caméra par rapport au robot, sans dépasser la limite minimale de 20
+        self.camDist = max(20, self.camDist - 50) 
 
     def zoomOut(self):
+        """Éloigne la caméra du robot."""
+        # Augmente la distance de la caméra par rapport au robot
         self.camDist += 50
 
     def deplaceRobot(self):
+        """Met à jour la position du robot dans l'environnement 3D."""
+        
         dt = globalClock.getDt()  # Obtient l'intervalle de temps écoulé depuis la dernière frame
         self.monde.robot.update()  # Met à jour la position et la direction du robot
 
@@ -85,79 +100,100 @@ class MyRobot(ShowBase, DirectObject):
         # Envoyer un signal pour mettre à jour les coordonnées
         self.messenger.send("update_coordinates", [self.monde.robot.x, self.monde.robot.y])
 
-    def rotationRobot(self):
-        self.robot.setH(self.robot,((self.monde.robot.dir)*180)/(math.pi))
-
     def afficheRobot(self):
+        """Charge et affiche le modèle 3D du robot."""
         # Charger le modèle de l'objet
         self.robot = self.loader.loadModel("src/graphique/model/robot")
+        # Positionner le modèle à la position du robot dans le monde
         self.robot.setPos(self.monde.robot.x, self.monde.robot.y, self.monde.robot.z)
-        self.robot.setH(self.robot,(self.monde.robot.dir * 180) / math.pi)
+        # Définir l'orientation du robot en fonction de sa direction
+        self.robot.setH(self.robot, (self.monde.robot.dir * 180) / math.pi)
+        # Attacher le modèle du robot au rendu de la scène
         self.robot.reparentTo(self.render)
 
     def afficheObstacle(self):
-        # Charger le modèle de l'obstacle
+        """Charge et affiche le modèle 3D des obstacles."""
+        # Parcourir la liste des obstacles dans le monde
         for obstacle in self.monde.obstacles:
+            # Charger le modèle de l'obstacle
             obs = self.loader.loadModel("src/graphique/model/obstacle")
+            # Positionner l'obstacle à sa position dans le monde
             obs.setPos(obstacle.x, obstacle.y, obstacle.z)
+            # Attacher le modèle de l'obstacle au rendu de la scène
             obs.reparentTo(self.render)
 
     def afficheEnvironnement(self):
+        """Charge et affiche le modèle 3D de l'environnement."""
         # Charger le modèle de l'environnement
         env = self.loader.loadModel("src/graphique/model/area")
+        # Attacher le modèle de l'environnement au rendu de la scène
         env.reparentTo(self.render)
 
     def setInitialCameraPosition(self):
-        # Calculate the initial camera's position
+        """Configure la position initiale de la caméra."""
+       # Calculer la position initiale de la caméra
         camX = self.x + self.camDist * math.cos(math.radians(self.camPitch)) * math.sin(math.radians(self.camAngle))
         camY = self.y + self.camDist * math.cos(math.radians(self.camPitch)) * -math.cos(math.radians(self.camAngle))
         camZ = self.z + self.camDist * math.sin(math.radians(self.camPitch))
 
+        # Définir la position de la caméra
         self.camera.setPos(camX, camY, camZ)
+        # Orienter la caméra pour qu'elle regarde vers le point spécifié
         self.camera.lookAt(Point3(self.x, self.y, self.z))
 
     def update3D(self, task):
-        self.deplaceRobot()
+        """Met à jour la vue 3D."""
+        # Déplacer le robot
+        self.deplaceRobot()  # Appelle une fonction pour déplacer le robot dans la scène.
 
-        # Update camera rotation
+        # Mettre à jour la rotation de la caméra si le contrôle de la caméra est activé et la souris est détectée.
         if self.cameraControl and self.mouseWatcherNode.hasMouse():
+            # Obtenir les nouvelles positions de la souris
             newMouseX = self.mouseWatcherNode.getMouseX()
             newMouseY = self.mouseWatcherNode.getMouseY()
 
+            # Calculer le changement dans la position de la souris
             deltaX = newMouseX - self.mouseX
             deltaY = newMouseY - self.mouseY
 
-            self.camAngle += deltaX * 100  # Adjust sensitivity as needed
+            # Ajuster l'angle de la caméra en fonction du mouvement de la souris avec une sensibilité donnée.
+            self.camAngle += deltaX * 100  # Sensibilité ajustable selon les besoins.
             self.camPitch -= deltaY * 100
-            self.camPitch = max(-89, min(89, self.camPitch))  # Limit the vertical angle
+            # Limiter l'angle vertical entre -89 et 89 degrés.
+            self.camPitch = max(-89, min(89, self.camPitch))
 
+            # Mettre à jour les positions de la souris.
             self.mouseX = newMouseX
             self.mouseY = newMouseY
 
-        # Calculate the camera's position
+        # Calculer la position de la caméra en fonction de l'angle de la caméra et de la distance par rapport au point spécifié.
         camX = self.x + self.camDist * math.cos(math.radians(self.camPitch)) * math.sin(math.radians(self.camAngle))
         camY = self.y + self.camDist * math.cos(math.radians(self.camPitch)) * -math.cos(math.radians(self.camAngle))
         camZ = self.z + self.camDist * math.sin(math.radians(self.camPitch))
 
+        # Définir la position de la caméra.
         self.camera.setPos(camX, camY, camZ)
+        # Orienter la caméra pour qu'elle regarde vers le point spécifié.
         self.camera.lookAt(Point3(self.x, self.y, self.z))
 
-        return task.cont  # Continuer l'appel de cette tâche à chaque frame
+        return task.cont  # Continuer l'appel de cette tâche à chaque frame.
 
-    def setupText(self):
-        # Créer un objet OnscreenText pour afficher les coordonnées du robot
-        self.coordText = OnscreenText(
-            text="",
-            pos=(-1.75, 0.95),
-            scale=0.07,
-            fg=(1, 1, 1, 1),  # Couleur du texte (blanc)
-            align=TextNode.ALeft,
-            mayChange=True  # Indique que le texte peut changer dynamiquement
-        )
+def setupText(self):
+    """Configure l'affichage du texte des coordonnées."""
+    # Créer un objet OnscreenText pour afficher les coordonnées du robot.
+    self.coordText = OnscreenText(
+        text="",  # Texte initial vide.
+        pos=(-1.75, 0.95),  # Position de l'affichage du texte à l'écran.
+        scale=0.07,  # Échelle du texte.
+        fg=(1, 1, 1, 1),  # Couleur du texte (blanc).
+        align=TextNode.ALeft,  # Alignement du texte à gauche.
+        mayChange=True  # Indique que le texte peut changer dynamiquement.
+    )
 
-        # Mettre à jour le texte des coordonnées à chaque frame
-        self.accept("update_coordinates", self.updateCoordinates)
-
+    # Mettre à jour le texte des coordonnées à chaque frame en écoutant l'événement "update_coordinates".
+    self.accept("update_coordinates", self.updateCoordinates)
+    
     def updateCoordinates(self, x, y):
+        """Met à jour le texte affichant les coordonnées du robot."""
         # Mettre à jour le texte avec les nouvelles coordonnées
         self.coordText.setText(f"x = {x:.2f}, y = {y:.2f}")
